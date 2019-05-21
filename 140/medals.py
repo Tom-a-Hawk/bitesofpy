2cb539a04a910d6edeca1165b106e7adfb10e05a
Tommy Englert <thomas.w.englert@uscis.dhs.gov>
2019-05-21 14:07:28 -0400

    delete unneccessary files

--- 140/medals.py
+++ 140/medals.py
@@ -5,6 +5,7 @@ data = "http://projects.bobbelderbos.com/data/summer.csv"
 
 
 def athletes_most_medals():
+
     df = pd.read_csv(data, header=0)
     w = df[df['Gender'] == 'Men'].groupby(['Athlete'])['Medal'].count().sort_values(ascending=False).head(1)
     x = df[df['Gender'] == 'Women'].groupby(['Athlete'])['Medal'].count().sort_values(ascending=False).head(1)
